a50174e481e7a34b84c4999376423d8ffb67ba32
Your Name <you@example.com>
2023-08-12 19:09:22 +0300

    best seller item part3

--- lib/Features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/Features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -21,22 +21,42 @@ class BestSallerListViewItem extends StatelessWidget {
                       image: AssetImage(AssetsData.testimage))),
             ),
           ),
-          SizedBox(
+          const SizedBox(
             width: 30,
           ),
           SizedBox(
             width: MediaQuery.of(context).size.width * .5,
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
+                const Text(
                   'Harry Potter and the Goblet of Fire',
                   maxLines: 2,
                   overflow: TextOverflow.ellipsis,
                   style: Styles.textStyle20,
+                ),
+                const SizedBox(
+                  height: 3,
+                ),
+                const Text(
+                  'J.K. Rowling',
+                  style: Styles.textStyle14,
+                ),
+                const SizedBox(
+                  height: 3,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      '19.99 €',
+                      style: Styles.textStyle20
+                          .copyWith(fontWeight: FontWeight.bold),
+                    )
+                  ],
                 )
               ],
             ),
-          )
+          ),
         ],
       ),
     );
