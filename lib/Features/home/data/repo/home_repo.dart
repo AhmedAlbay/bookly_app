410be533e9b6a384f8e7b81da5f051f4e6d43245
Your Name <you@example.com>
2023-08-30 18:25:32 +0300

    create home_repo and failure_class

--- lib/Features/home/data/repo/home_repo.dart
+++ lib/Features/home/data/repo/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/core/utils/errors/failure_class.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchBestSallerBooks();
-  fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSallerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
