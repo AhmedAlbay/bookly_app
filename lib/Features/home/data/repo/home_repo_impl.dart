84c4d12c0ca1372235eb1cc20bfa54be60a45b62
Your Name <you@example.com>
2023-09-10 12:51:26 +0300

    implement similar books

--- lib/Features/home/data/repo/home_repo_impl.dart
+++ lib/Features/home/data/repo/home_repo_impl.dart
@@ -53,4 +53,25 @@ class HomeRepoImpl implements HomeRepo {
       );
     }
   }
+  
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSimilardBooks({required String category}) async{
+try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=relevance&q=computer science');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServiceFailure.fromDioException(e));
+      }
+      return left(
+        ServiceFailure(e.toString()),
+      );
+    }
+  }
 }
